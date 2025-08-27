3202e1932ee446dcaa0d34fe23a37f0d7c251861
Salsa101 <afridasalsabila3@gmail.com>
2025-08-27 21:08:05 +0700

    adding ip app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -15,6 +15,6 @@ app.get("/", (req, res) => {
   res.send("API is running!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
