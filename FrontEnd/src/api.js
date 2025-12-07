813c73b7c839086b6f3da64892d879c666518991
Salsa101 <afridasalsabila3@gmail.com>
2025-12-07 20:02:15 +0700

    update api

--- FrontEnd/src/api.js
+++ FrontEnd/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://10.0.2.2:3000';
+const API_BASE_URL = 'project-skinsense-production.up.railway.app';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
